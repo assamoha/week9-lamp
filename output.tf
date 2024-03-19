90e443f8cd967d3803b13e6c113c546d9c4e3d20
assamoha <assamohalinux@gmail.com>
2024-03-19 17:33:32 -0400

    lightsail

---
+++ output.tf
@@ -0,0 +1,10 @@
+output "public" {
+  value = aws_lightsail_instance.server2.public_ip_address
+}
+output "username" {
+  value = aws_lightsail_instance.server2.username
+}
+
+output "ssh-command" {
+  value = "ssh -i ${local_file.ssh_key.filename} ${aws_lightsail_instance.server2.username}@${aws_lightsail_instance.server2.public_ip_address}"
+}
